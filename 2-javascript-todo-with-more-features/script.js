95d76a02984fd79e35c38d599eae9c2e66fa228f
OzanOcak <ozanocak@gmail.com>
2024-08-23 14:54:22 -0400

    javascript todo-2 end file

--- 2-javascript-todo-with-more-features/script.js
+++ 2-javascript-todo-with-more-features/script.js
@@ -2,6 +2,7 @@
 const todoInput = document.getElementById("todoInput");
 const addTodoBtn = document.getElementById("addTodoBtn");
 const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -10,12 +11,21 @@ function renderTodos() {
   todos.forEach((todo, index) => {
     const li = document.createElement("li");
     li.textContent = todo.text;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => toggleTodoCompletion(index));
+
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener("click", () => deleteTodo(index));
+
+    li.appendChild(checkbox);
     li.appendChild(deleteBtn);
     todoList.appendChild(li);
   });
+  updateTodoCount();
 }
 
 function addTodo() {
@@ -32,6 +42,18 @@ function deleteTodo(index) {
   todos.splice(index, 1);
   localStorage.setItem("todos", JSON.stringify(todos));
   renderTodos();
+}
+
+function toggleTodoCompletion(index) {
+  todos[index].completed = !todos[index].completed;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos();
+}
+
+function updateTodoCount() {
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const activeTodos = todos.length - completedTodos;
+  todoCount.textContent = `Active: ${activeTodos}, Completed: ${completedTodos}`;
 }
 
 addTodoBtn.addEventListener("click", addTodo);

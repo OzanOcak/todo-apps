3c1f9dd8e97cdb61ff6367a0080f1551d4f1e827
OzanOcak <ozanocak@gmail.com>
2024-09-14 11:43:10 -0400

    ui of task list

--- 8-react-todo-ts/src/App.tsx
+++ 8-react-todo-ts/src/App.tsx
@@ -1,9 +1,30 @@
-import {} from 'react';
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
 
 function App() {
+  const tasks: Task[] = [
+    { id: 1, title: 'Learn React', isCompleted: true },
+    { id: 2, title: 'Create React Tutorial', isCompleted: true },
+    { id: 3, title: 'Develop React Project', isCompleted: false },
+  ];
   return (
     <>
-      <div>hello world</div>
+      <h1>Task</h1>
+      <label className="sr-only" htmlFor="task-imput">
+        Add Task
+      </label>
+      <input type="task-input" />
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </>
   );
 }

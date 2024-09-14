c321ec51a14d277caba2a6230352fbe3256adfbb
OzanOcak <ozanocak@gmail.com>
2024-09-14 11:57:10 -0400

    8 - adding task

--- 8-react-todo-ts/src/App.tsx
+++ 8-react-todo-ts/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type Priority = 'p1' | 'p2' | 'p3';
 
 type Task = {
@@ -8,18 +10,30 @@ type Task = {
 };
 
 function App() {
-  const tasks: Task[] = [
+  const [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: 'Learn React', isCompleted: true },
     { id: 2, title: 'Create React Tutorial', isCompleted: true },
     { id: 3, title: 'Develop React Project', isCompleted: false },
-  ];
+  ]);
+
+  const [taskItem, setTaskItem] = useState<string>('');
+
+  function handleAddItem() {
+    setTasks([...tasks, { id: 1, title: taskItem, isCompleted: false }]);
+  }
+
   return (
     <>
       <h1>Task</h1>
       <label className="sr-only" htmlFor="task-imput">
-        Add Task
+        Add Task:
       </label>
-      <input type="task-input" />
+      <input
+        value={taskItem}
+        onChange={(e) => setTaskItem(e.target.value)}
+        id="task-input"
+      />
+      <button onClick={handleAddItem}>Add</button>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>{task.title}</li>
